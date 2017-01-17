4751e044974f913306fcff16080feaa159b21bee
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-17 09:53:42 -0800

    complete js

--- src/lab03-tutorial.js
+++ src/lab03-tutorial.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function sum(n){
 	if (n==0) { // base case
 	     return 0;
@@ -8,3 +10,6 @@ function sum(n){
 
 //example:
 //	sum(4) => 4 + 3 + 2 + 1 + 0 = 10
+
+let n = 4;
+console.log(`The sum of all numbers from 0-${n} is ${sum(n)}`);
